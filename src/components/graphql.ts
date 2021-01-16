0f220a2cad4bacf5b76851273485e7548d2deb6b
Juri Adams <juri@adams.sh>
2021-01-16 18:24:03 +0100

    🚧 Disable Cache

--- src/components/graphql.ts
+++ src/components/graphql.ts
@@ -34,6 +34,14 @@ export default class GraphQL {
         this.client = new ApolloClient({
             cache: this.cache,
             link: this.link,
+            defaultOptions: {
+                watchQuery: {
+                    fetchPolicy: "no-cache",
+                },
+                query: {
+                    fetchPolicy: "no-cache",
+                },
+            },
         });
     }
 }
